da2a3cf270295e71590687008f488984ed7ea9f7
Егор Васин <egor.vasin1011@gmail.com>
2023-06-28 21:20:09 +0300

    1

--- src/test12.java
+++ src/test12.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class test12 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        float x = scanner.nextInt();
+        float y = scanner.nextInt();
+        int day=1;
+        while (x<y) {
+            day++;
+            x *= 1.1;
+        }
+        System.out.println( day + " " + x);
     }
 }
